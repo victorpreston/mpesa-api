30264d803c13284cdca784c1992947316971fbf5
Victor Preston <prestonvictor25@gmail.com>
2026-02-02 16:57:42 +0300

    chore: init commit && setup

---
+++ src/test/java/com/grainindustries/mpesa_c2b_api/MpesaC2bApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grainindustries.mpesa_c2b_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaC2bApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
