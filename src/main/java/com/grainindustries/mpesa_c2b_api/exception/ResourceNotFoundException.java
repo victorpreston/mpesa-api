9a4f2d3db24b9494dd2589ee2e4a3385f54b6599
Victor Preston <prestonvictor25@gmail.com>
2026-02-05 06:44:27 +0300

    Add custom exception classes for error handling

---
+++ src/main/java/com/grainindustries/mpesa_c2b_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.grainindustries.mpesa_c2b_api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
