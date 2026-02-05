154ab82a9828f8e7864f1b27ffdd117d4afa7528
Victor Preston <prestonvictor25@gmail.com>
2026-02-05 07:12:30 +0300

    fix: database schema validation

--- src/main/java/com/grainindustries/mpesa_c2b_api/entity/MpesaTransaction.java
+++ src/main/java/com/grainindustries/mpesa_c2b_api/entity/MpesaTransaction.java
@@ -58,7 +58,7 @@ public class MpesaTransaction {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
     
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
     
     @PrePersist
